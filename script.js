c0d5327fac5326f0df123b221b6664cfb645ed74
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-30 02:27:37 +0530

    screen added

--- script.js
+++ script.js
@@ -5,17 +5,29 @@ const container = document.getElementById("container");
 async function getAJoke() {
     const response = await fetch(APIURL);
     const responseData = await response.json();
+    console.log(responseData);
 
     const jokeCard = document.createElement("div");
     jokeCard.classList.add("joke-card");
+    jokeCard.innerHTML = "";
     jokeCard.innerHTML = `
-    <div class="joke-card" id="joke-card">
-                <h1>Joke</h1>
-                <p>This is </p>
-            </div>
+                <i class="fas fa-sync-alt rotating hidden" id="rotate"></i>
+                <p>${responseData.setup}</p>
+                <p>${responseData.punchline}</p>
+            <button class="btn" id="joke-next">GET JOKE</button>
     `;
 
+    container.innerHTML = "";
     container.appendChild(jokeCard);
+
+    const jokeBtn = document.getElementById("joke-next");
+    const rotateIcon = document.getElementById("rotate");
+
+    jokeBtn.addEventListener("click", () => {
+        jokeCard.innerHTML = `
+        <i class="fas fa-sync-alt rotating active" id="rotate"></i>`;
+        getAJoke();
+    });
 }
 
 getAJoke();
